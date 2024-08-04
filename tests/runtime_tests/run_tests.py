f11af7d019a19958d366a43889405cf7044fe702
lis05 <lis05st@gmai.com>
2024-08-04 13:15:16 +0200

    tests

---
+++ tests/runtime_tests/run_tests.py
@@ -0,0 +1,56 @@
+#!/bin/python3
+
+import glob
+import subprocess
+
+tests = glob.glob("*.ctn")
+succeeded = 0
+failed = 0
+
+for file in tests:
+    fd = open(file, "r")
+    src = fd.read()
+    fd.close()
+    lines = src.splitlines()
+    words = src.split()
+
+    desc = lines[0].replace("//", "").strip()
+
+
+    expected_output = []
+
+    try:
+        output_first = words.index("BEGIN_MATCH_WORDS")
+        output_last = words.index("END_MATCH_WORDS")
+        expected_output = words[output_first + 1: output_last]
+    except ValueError:
+        pass
+
+    proc = subprocess.run(["/home/lis05/Projects/Cotton/build/cotton_int/cotton_int", file], capture_output=True)
+    if proc.returncode != 0:
+        print(f"❌ {desc} - FAILED: exit code {proc.returncode}\nErrors:")
+        print(proc.stderr.decode())
+        failed = failed + 1
+        continue
+
+    proc_output = proc.stdout.decode().split()
+
+    if len(expected_output) != len(proc_output):
+        print(f"❌ {desc} - FAILED: produced %s words, expected %s" % (len(proc_output), len(expected_output)))
+        failed = failed + 1
+        continue
+
+    for i in range(len(expected_output)):
+        if expected_output[i] != proc_output[i]:
+            print(f"❌ {desc} - FAILED: words mismatch at position %s: produced %s, expected %s" % (i, proc_output[i], expected_output[i]))
+            failed = failed + 1
+            continue
+    print(f"✅ {desc} - SUCCESS")
+    succeeded = succeeded + 1
+
+print("SUCCEEDED %s, FAILED %s (total %s)" % (succeeded, failed, succeeded + failed))
+
+    
+
+
+    
